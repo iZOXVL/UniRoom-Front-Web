7feb46d502dd935507c578f3835ad1de67cc2df0
Armando VL <103782831+iZOXVL@users.noreply.github.com>
2024-10-22 12:00:53 -0600

    UR: Configuración inicial para obtener las habitaciones v2

--- src/components/Rooms/hooks/useGetRoom.ts
+++ src/components/Rooms/hooks/useGetRoom.ts
@@ -1,27 +1,55 @@
-"use client";
-import { useEffect, useState } from "react";
-import { Room } from "@/types/Room";
+import { useState } from "react";
+import { useToast } from "@chakra-ui/react";
+import { useCurrentUser } from "@/hooks/use-current-user";
 
-export const useGetRooms = () => {
-  const [habitaciones, setRooms] = useState<Room[]>([]);
-  const [loading, setLoading] = useState<boolean>(true);
-  const [error, setError] = useState<Error | null>(null);
+interface GetRoomsParams {
+  pageNumber: number;
+  pageSize: number;
+}
 
-  useEffect(() => {
-    const fetchRooms = async () => {
-      try {
-        const response = await fetch("https://uruniroom.azurewebsites.net/api/Rooms/GetRoomLandlord");
-        const data = await response.json();
-        setRooms(data);
-      } catch (error) {
-        setError(error as Error);
-      } finally {
-        setLoading(false);
-      }
-    };
+const useGetRooms = () => {
+  const user = useCurrentUser();
+  const userToken = user?.JwtToken;
+  const [isLoading, setIsLoading] = useState(false);
+  const toast = useToast();
 
-    fetchRooms();
-  }, []);
+  const getRooms = async (params: GetRoomsParams): Promise<any | null> => {
+    setIsLoading(true);
+    
+    try {
+      const response = await fetch("https://uruniroom.azurewebsites.net/api/Rooms/GetRoomLandLord", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          token: userToken,
+          pageNumber: 1,
+          pageSize: 10,
+        }),
+      });
 
-  return { habitaciones, loading, error };
+      if (!response.ok) throw new Error("Error al obtener las habitaciones.");
+      
+      const data = await response.json();
+      return data;
+
+    } catch (error) {
+      console.error(error);
+      toast({
+        title: "Error",
+        description: "Hubo un problema al obtener las habitaciones.",
+        status: "error",
+        duration: 9000,
+        isClosable: true,
+      });
+      return null;
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return { getRooms, isLoading };
 };
+
+export default useGetRooms;
