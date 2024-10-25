b0fa4d2d87f4d4de95c0f9f4efb39110cbe44a6f
namugf-admin <gigdemgalvan@gmail.com>
2024-10-25 14:48:49 -0600

    UR : Implementación de estilos

--- src/components/Admin/chat/chat.tsx
+++ src/components/Admin/chat/chat.tsx
@@ -8,6 +8,9 @@ import { io } from "socket.io-client"; // Asegúrate de tener socket.io-client i
 import { BiSearch, BiSend } from "react-icons/bi";
 import { IoAttach } from "react-icons/io5";
 import { SlEmotsmile } from "react-icons/sl";
+import { DotLottiePlayer } from '@dotlottie/react-player';
+import '@dotlottie/react-player/dist/index.css';
+import EmojiPicker, { EmojiClickData } from "emoji-picker-react";
 
 const Chat: React.FC = () => {
   const { chats, loading, error, userToken, userName, userId } = useGetApprovedChats(); // Obtener chats aprobados
@@ -16,26 +19,33 @@ const Chat: React.FC = () => {
   const [loadingMessages, setLoadingMessages] = useState(false); // Estado de carga de mensajes
   const [errorMessages, setErrorMessages] = useState<string | null>(null); // Estado de error en mensajes
   const [newMessage, setNewMessage] = useState<string>(""); // Estado para el nuevo mensaje
+  const [isPickerVisible, setIsPickerVisible] = useState(false); 
 
-  
 
   // Conexión al socket
   const socket = io("https://uniroom-backend-services.onrender.com"); // Asegúrate de usar la URL correcta
 
   // Crear una referencia para el contenedor de mensajes
-  const messagesEndRef = useRef<HTMLDivElement>(null);
+  const messagesEndRef = useRef<HTMLDivElement>(null)
 
   // Función para hacer scroll al final del contenedor de mensajes
   const scrollToBottom = () => {
     messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
   };
 
+ // Actualiza la función para que solo reciba el parámetro de emoji
+const onEmojiClick = (emojiObject: EmojiClickData) => {
+  setNewMessage((prevMessage) => prevMessage + emojiObject.emoji);
+  setIsPickerVisible(false); // Cierra el picker después de seleccionar el emoji
+};
+
+
   useEffect(() => {
     if (selectedChatId) {
       socket.emit("joinChat", selectedChatId); // Unirse al chat seleccionado
       fetchMessages(selectedChatId);
     }
-  
+
     // Escuchar mensajes entrantes
     socket.on("message", (message) => {
       console.log("Nick", message);
@@ -114,7 +124,7 @@ const Chat: React.FC = () => {
         }),
       });
 
-      
+
     } catch (error) {
       console.error("Error al enviar el mensaje:", error);
     }
@@ -132,13 +142,13 @@ const Chat: React.FC = () => {
     <>
       <Breadcrumb pageName="Conversaciones" />
       <div className="h-[calc(93vh-186px)] overflow-hidden sm:h-[calc(93vh-174px)]">
-        <div className="h-full rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark xl:flex">
+        <div className="h-full rounded-sm border border-stroke bg-white shadow-default dark:border-dark-3 dark:bg-gray-dark xl:flex">
           <div className="hidden h-full flex-col xl:flex xl:w-1/4">
             {/* Chat List Header */}
-            <div className="sticky border-b border-stroke px-6 py-7.5 dark:border-strokedark">
+            <div className="sticky border-b border-stroke px-6 py-7.5 dark:border-dark-3 dark:bg-gray-dark">
               <h3 className="text-lg font-medium text-black dark:text-white 2xl:text-xl">
                 Chats activos
-                <span className="rounded-md border-[.5px] ml-4 border-stroke bg-gray-2 px-2 py-0.5 text-base font-medium text-black dark:border-strokedark dark:bg-boxdark-2 dark:text-white">
+                <span className="rounded-md border-[.5px] ml-4 border-stroke bg-gray-2 px-2 py-0.5 text-base font-medium text-black dark:border-dark-3 dark:bg-gray-dark dark:text-white">
                   {chats.length}
                 </span>
               </h3>
@@ -147,7 +157,7 @@ const Chat: React.FC = () => {
               <form className="relative mb-7">
                 <input
                   type="text"
-                  className="w-full rounded border border-stroke bg-gray-2 py-2.5 pl-5 pr-10 text-sm outline-none focus:border-primary dark:border-strokedark dark:bg-boxdark-2"
+                  className="w-full rounded border border-stroke bg-gray-2 py-2.5 pl-5 pr-10 text-sm outline-none focus:border-primary dark:border-dark-3 dark:bg-gray-dark"
                   placeholder="Buscar..."
                 />
                 <button className="absolute right-4 top-1/2 -translate-y-1/2">
@@ -158,7 +168,7 @@ const Chat: React.FC = () => {
                 {chats.map((chat) => (
                   <div
                     key={chat.id}
-                    className="flex cursor-pointer items-center rounded px-4 py-2 hover:bg-gray-2 dark:hover:bg-strokedark"
+                    className="flex cursor-pointer items-center rounded px-4 py-2 hover:bg-gray-2 dark:hover:bg-dark"
                     onClick={() => handleChatSelect(chat.id)}
                   >
                     <div className="relative mr-3.5 h-11 w-full max-w-11 rounded-full">
@@ -169,7 +179,7 @@ const Chat: React.FC = () => {
                         width={44}
                         height={44}
                       />
-                      <span className="absolute bottom-0 right-0 block h-3 w-3 rounded-full border-2 border-gray-2 bg-success"></span>
+                      <span className="absolute bottom-0 right-0 block h-3 w-3 rounded-full border-2 border-gray-2 bg-success "></span>
                     </div>
                     <div className="w-full">
                       <h5 className="text-sm font-medium text-black dark:text-white">
@@ -184,9 +194,9 @@ const Chat: React.FC = () => {
           </div>
 
           {/* Chat Box */}
-          <div className="flex h-full flex-col border-l border-stroke dark:border-strokedark xl:w-3/4">
+          <div className="flex h-full flex-col border-l border-stroke dark:border-dark-3 dark:bg-gray-dark xl:w-3/4">
             {selectedChatId && (
-              <div className="sticky flex items-center justify-between border-b border-stroke px-6 py-4.5 dark:border-strokedark">
+              <div className="sticky flex items-center justify-between border-b border-stroke px-6 py-4.5 dark:border-dark-3 dark:bg-gray-dark">
                 <div className="flex items-center">
                   <div className="mr-4.5 h-13 w-full max-w-13 overflow-hidden rounded-full">
                     <Image
@@ -202,8 +212,8 @@ const Chat: React.FC = () => {
                       {chats.find(chat => chat.id === selectedChatId)?.participantDetails[1]?.name}
                     </h5>
                     <p>
-                    {chats.find(chat => chat.id === selectedChatId)?.roomDetails.title}
-                    </p>    
+                      {chats.find(chat => chat.id === selectedChatId)?.roomDetails.title}
+                    </p>
                   </div>
                 </div>
               </div>
@@ -215,49 +225,68 @@ const Chat: React.FC = () => {
               <div className="text-center text-red-500">Error: {errorMessages}</div>
             ) : (
               <div className="p-6 flex-grow overflow-auto">
-  {messages.length > 0 ? (
-    <ul className="space-y-4">
-      {messages.map((message, index) => (
-        <li
-          key={index}
-          className={`flex flex-col ${message.nickname === userName ? 'items-end' : 'items-start'}`}
-        >
-          <div
-            className={`inline-block mb-1.5 rounded-2xl px-5 py-4 ${
-              message.nickname === userName
-                ? 'bg-primary text-white rounded-br-none'
-                : 'bg-gray-200 dark:bg-strokedark rounded-tl-none'
-            }`}
-          >
-            <p>{message.content}</p>
-          </div>
-          <p
-            className={`text-xs text-gray-500 ${message.nickname === userName ? 'text-right' : 'text-left'}`}
-          >
-            {message.nickname === userName ? 'Tú' : message.nickname}
-          </p>
-        </li>
-      ))}
-      {/* Este div vacío sirve como ancla para el scroll */}
-      <div ref={messagesEndRef}></div>
-    </ul>
-  ) : (
-    <p className="text-center text-gray-500">No hay mensajes en este chat.</p>
-  )}
-</div>
+                {messages.length > 0 ? (
+                  <ul className="space-y-4">
+                    {messages.map((message, index) => (
+                      <li
+                        key={index}
+                        className={`flex flex-col ${message.nickname === userName ? 'items-end' : 'items-start'}`}
+                      >
+                        <div
+                          className={`inline-block mb-1.5 rounded-2xl px-5 py-4 ${message.nickname === userName
+                              ? 'bg-primary text-white rounded-br-none'
+                              : 'bg-gray-200 dark:bg-strokedark rounded-tl-none'
+                            }`}
+                        >
+                          <p>{message.content}</p>
+                        </div>
+                        <p
+                          className={`text-xs text-gray-500 ${message.nickname === userName ? 'text-right' : 'text-left'}`}
+                        >
+                          {message.nickname === userName ? 'Tú' : message.nickname}
+                        </p>
+                      </li>
+                    ))}
+                    {/* Este div vacío sirve como ancla para el scroll */}
+                    <div ref={messagesEndRef}></div>
+                  </ul>
+                ) : (
+                  <div className="flex items-center justify-center bg-transparent dark:bg-transparent flex-col">
+                    <DotLottiePlayer
+                      src="/lotties/chat.lottie"
+                      autoplay
+                      speed={0.55}
+                      style={{
+                        width: '80vw', // Toma el 80% del ancho de la ventana 
+                        maxWidth: '500px', // Máximo de 500px para pantallas más grandes
+                        height: 'auto',    // Ajusta la altura automáticamente
+                        maxHeight: '400px' // Máximo de 400px de altura
+
+                      }}
+                    >
+                    </DotLottiePlayer>
+                    <h1 className="text-3xl font-bold text-black dark:text-slate-200 text-center">
+                      Conversa con tus futuros inquilinos
+                    </h1>
+                    <p className="text-lg text-gray-600 dark:text-gray-300 mt-2 text-center">
+                      Conéctate con estudiantes interesados, responde sus preguntas y facilita el proceso de renta de tus habitaciones.
+                    </p>
+                  </div>
+                )}
+              </div>
 
             )}
 
             {/* Chat Input */}
             {selectedChatId && (
-              <div className="border-t border-stroke p-5 dark:border-strokedark">
+              <div className="border-t border-stroke p-5 dark:border-dark-3 dark:bg-gray-dark">
                 <form className="relative" onSubmit={handleSendMessage}>
                   <input
                     type="text"
                     value={newMessage}
                     onChange={(e) => setNewMessage(e.target.value)}
                     placeholder="Escribe tu mensaje..."
-                    className="w-full rounded border border-stroke bg-gray-2 py-3.5 pl-5 pr-20 outline-none focus:border-primary dark:border-strokedark dark:bg-boxdark-2 dark:text-white"
+                    className="w-full rounded-[7px] border-[1.5px] bg-slate-50  border-gray-4 bg-transparent px-5.5 py-3 text-dark outline-none transition focus:border-primary dark:focus:border-primary active:border-primary dark:border-dark-3 dark:bg-dark-2 dark:text-white dark:active:border-primary"
                   />
                   <button className="absolute right-4 top-1/2 -translate-y-1/2" type="submit">
                     <BiSend size={20} />
@@ -265,9 +294,18 @@ const Chat: React.FC = () => {
                   <button className="absolute right-12 top-1/2 -translate-y-1/2" type="button">
                     <IoAttach size={20} />
                   </button>
-                  <button className="absolute right-20 top-1/2 -translate-y-1/2" type="button">
+                  <button
+                    className="absolute right-20 top-1/2 -translate-y-1/2"
+                    type="button"
+                    onClick={() => setIsPickerVisible(!isPickerVisible)} // Alterna el picker
+                  >
                     <SlEmotsmile size={20} />
                   </button>
+                  {isPickerVisible && (
+                    <div className="absolute bottom-full right-0 mb-2">
+                      <EmojiPicker onEmojiClick={onEmojiClick} />
+                    </div>
+                  )}
                 </form>
               </div>
             )}
